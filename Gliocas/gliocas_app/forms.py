6cc235c2b1cd3002ce3ce318e34bec13b2e089b5
Richard Nemeth <2333065n@student.gla.ac.uk>
2019-03-03 22:35:16 +0000

    added login functionality

--- Gliocas/gliocas_app/forms.py
+++ Gliocas/gliocas_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from gliocas_app.models import UserProfile
+#from gliocas_app.models import UserProfile
 
 class UserForm(forms.ModelForm):
 
--- Gliocas/gliocas_app/views.py
+++ Gliocas/gliocas_app/views.py
@@ -5,7 +5,8 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from gliocas_app.forms import UserForm
 from django.contrib.auth.models import User
-from gliocas_app.models import Subject, Course, Question, UserProfile
+from gliocas_app.models import Subject, Course, Question
+
 
 def index(request):
     context_dict = {}
@@ -94,10 +95,10 @@ def register(request):
                 user.set_password(user.password)
                 user.save()
 
-                profile = UserProfile()
-                profile.user = user
+                #profile = UserProfile()
+                #profile.user = user
 
-                profile.save()
+                #profile.save()
 
                 registered = True
 
@@ -113,8 +114,29 @@ def register(request):
 
 
 def user_login(request):
-    context_dict = {}
-    return render(request,'gliocas_app/login.html', context = context_dict)
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+
+            if user.is_active:
+
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request, 'gliocas_app/login.html', {})
 
 @login_required
 def user_logout(request):
