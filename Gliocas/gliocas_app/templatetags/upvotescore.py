11eb346e828e3e6dfedc90e0e97b55a3aed43da2
Samuel Gurský <samkogursky@gmail.com>
2019-03-21 01:35:04 +0100

    added templatetags to display answer and reply scores

---
+++ Gliocas/gliocas_app/templatetags/upvotescore.py
@@ -0,0 +1,16 @@
+from gliocas_app.models import Subject, Course, Question, UpvoteQuestion, UpvoteAnswer, UpvoteReply, Subject, Answer, Reply, Followed
+from django import template
+
+register = template.Library()
+
+def score(votes):
+    score = sum([ 1 if vote.positive else -1 for vote in votes ])
+    return score
+
+@register.simple_tag
+def answerscore(answer):
+    return score(UpvoteAnswer.objects.filter(answer = answer))
+
+@register.simple_tag
+def replyscore(reply):
+    return score(UpvoteReply.objects.filter(reply = reply))
